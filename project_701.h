397d0fec2173de7b384219157da8c2a50daacac6
Yuzig <gyz315@gmail.com>
2020-07-03 09:34:22 -0700

    initiate project

---
+++ project_701.h
@@ -0,0 +1,18 @@
+//
+//  project_701.h
+//  project 701
+//
+//  Created by Carol Gao on 7/3/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for project_701.
+FOUNDATION_EXPORT double project_701VersionNumber;
+
+//! Project version string for project_701.
+FOUNDATION_EXPORT const unsigned char project_701VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <project_701/PublicHeader.h>
+
+
